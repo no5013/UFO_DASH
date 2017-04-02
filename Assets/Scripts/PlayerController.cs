ac84acf07d9c6284fde09497d2a793af30416241
Pongsakorn Somsri <pongsakorns@gmail.com>
2017-04-03 03:15:07 +0700

    Ui and score count

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public Text countText;
+	public Text winText;
+
 	private Rigidbody2D rb;
+	private int count;
 
 	void Start(){
 		rb = GetComponent<Rigidbody2D> ();
+		winText.text = "";
+		count = 0;
+	}
+
+	void SetCountText () {
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 1) {
+			winText.text = "YOU WIN";
+		}
 	}
 
 	// Update is called once per frame
@@ -23,6 +37,8 @@ public class PlayerController : MonoBehaviour {
 		if (other.gameObject.CompareTag ("PickUp")) 
 		{
 			other.gameObject.SetActive (false);
+			count++;
+			SetCountText ();
 		}
 	}
 }
